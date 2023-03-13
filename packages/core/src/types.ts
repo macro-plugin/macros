8ca77c92f4594544f8920b16fe4cc5ec5c47f0cf
voorjaar <felitas@zoho.com>
2023-03-13 21:23:05 +0800

    feat: new config type

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,3 +1,4 @@
+import type { ParserOptions } from "@babel/parser";
 import type { Statement } from "estree";
 
 export type {
@@ -10,3 +11,9 @@ export type { ParserOptions } from "@babel/parser"
 export type { BaseNode } from "estree-walker"
 
 export type MacroPlugin = (ast: Statement, code: string) => Statement | string
+
+export type Config = {
+  global?: Record<string, MacroPlugin>,
+  labeled?: Record<string, MacroPlugin>,
+  parserOptions?: ParserOptions
+}
