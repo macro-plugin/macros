306beea6f10e68a6ea5f36c7e32868a27ac80e10
voorjaar <felitas@zoho.com>
2023-03-13 22:23:41 +0800

    feat: add handler type

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,5 +1,6 @@
+import type { BaseNode, Statement } from "estree";
+
 import type { ParserOptions } from "@babel/parser";
-import type { Statement } from "estree";
 
 export type {
   LabeledStatement,
@@ -10,10 +11,17 @@ export type { ParserOptions } from "@babel/parser"
 
 export type { BaseNode } from "estree-walker"
 
-export type MacroPlugin = (ast: Statement, code: string) => Statement | string
+export type Handler = {
+  skip: () => void;
+  remove: () => void;
+  replace: (node: BaseNode) => void;
+};
+
+export type LabeledMacro = (ast: Statement, code: string) => Statement | string
+export type GlobalMacro = (ast: BaseNode, handler: Handler, parent: BaseNode, prop: string, index: number) => void | BaseNode
 
 export type Config = {
-  global?: Record<string, MacroPlugin>,
-  labeled?: Record<string, MacroPlugin>,
+  global?: Record<string, GlobalMacro>,
+  labeled?: Record<string, LabeledMacro>,
   parserOptions?: ParserOptions
 }
