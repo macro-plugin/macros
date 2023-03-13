b773f88947aa26998bd1eec75a439d3d3bfc36ba
voorjaar <felitas@zoho.com>
2023-03-14 00:01:45 +0800

    fix: jest module mapper

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import("@jest/types").Config.InitialOptions} */
 module.exports = {
   transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
+  moduleNameMapper: Object.fromEntries(require("fs").readdirSync("./packages").map(i => [[`^@macro-plugin/${i}`], [`<rootDir>/packages/${i}/src`]])),
   collectCoverageFrom: ["packages/{!(tsconfig.json),}.test.ts", "packages/**/*.test.ts"],
 };
