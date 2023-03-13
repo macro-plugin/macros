129f6bcb502d6b416a043234735835c832c11c6e
voorjaar <felitas@zoho.com>
2023-03-13 22:55:24 +0800

    feat: add import handler types

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -5,16 +5,27 @@ import type { ParserOptions } from "@babel/parser";
 export type {
   LabeledStatement,
   Statement,
+  ImportDeclaration
 } from "estree"
 
 export type { ParserOptions } from "@babel/parser"
 
 export type { BaseNode } from "estree-walker"
 
+export type ImportSpecifier = {
+  name: string,
+  kind?: null | "type" | "default"
+}
+
 export type Handler = {
+  /** Skip this node, not handling */
   skip: () => void;
+  /** Remove this node */
   remove: () => void;
+  /** Replace matched node with new node, equals to `return node` */
   replace: (node: BaseNode) => void;
+  /** Import some package */
+  import: (specifiers: ImportSpecifier[], source: string, kind?: 'type' | 'value' | undefined | null) => void
 };
 
 export type LabeledMacro = (ast: Statement, code: string) => Statement | string
