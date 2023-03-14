1b87265d1386f869519d396f1e5244b24a98d781
voorjaar <felitas@zoho.com>
2023-03-14 14:43:18 +0800

    feat: export createTrackPlugin

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,8 @@
+import createTrackPlugin from "./track";
+
 export { transform } from "./transform";
 export { generate } from "./generate";
 export { parse } from "./parse";
 export { walk } from "./walk";
+export { createTrackPlugin };
 export * from "./types";
