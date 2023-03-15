957b2cb2e10066fcd4fd4618750bbd545c55b1c3
voorjaar <felitas@zoho.com>
2023-03-15 17:21:35 +0800

    test: qwik signal macros

---
+++ packages/qwik/tests/signal.test.ts
@@ -0,0 +1,28 @@
+import { signal } from "@macro-plugin/qwik"
+import { transform } from "@macro-plugin/core"
+
+test("qwik signal", () => {
+  const code = `
+    signal: {
+      var count = 0
+    }
+
+    count += 1
+  `
+  expect(transform(code, { global: { signal } }).code).toMatchSnapshot()
+})
+
+test("qwik signal in function name", () => {
+  const code = `
+  signal: {
+    var count = 0
+  }
+  function abc(count) {
+    count = 3
+  }
+  count += 2
+  `
+
+  expect(transform(code, { global: { signal } }).code).toMatchSnapshot();
+});
+
