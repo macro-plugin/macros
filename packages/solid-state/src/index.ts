c30292864b2923123c31bdb2b61e5a208115efbb
voorjaar <felitas@zoho.com>
2023-03-15 12:13:30 +0800

    fix: export all macros

--- packages/solid-state/src/index.ts
+++ packages/solid-state/src/index.ts
@@ -1,1 +1,4 @@
-export { default as signal } from './signal'
+export { default as signal } from './signal';
+export { default as store } from './store';
+export { effect } from './effect';
+export { onMount, onCleanup, onError } from './lifecycle';
