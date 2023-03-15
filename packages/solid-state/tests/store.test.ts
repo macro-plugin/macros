d35aa7d55cc6b30d7b849d71c5a5b182050093c5
voorjaar <felitas@zoho.com>
2023-03-15 12:13:00 +0800

    test: store macros

---
+++ packages/solid-state/tests/store.test.ts
@@ -0,0 +1,25 @@
+import { store } from "@macro-plugin/solid-state"
+import { transform } from "@macro-plugin/core"
+
+test("solid store macro", () => {
+  const code = `
+    store: {
+      var todos = [
+        { id: 1, title: "Thing I have to do", done: false },
+        { id: 2, title: "Learn a New Framework", done: false },
+      ]
+    }
+
+    console.log(todos)
+
+    todos = [
+      { id: 2, title: "Thing I have to do", done: false },
+      { id: 3, title: "Learn a New Framework", done: false },
+    ]
+  `
+
+  // todos[0] = { id: 1, title: "Thing I have to do", done: false }
+  // todos[1].id = 3;
+
+  expect(transform(code, { global: { store } }).code).toMatchSnapshot();
+})
