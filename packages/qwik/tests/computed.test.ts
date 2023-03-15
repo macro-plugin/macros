90049331fe7c960dfe7de4e622bc3ff7ff5d5c0b
voorjaar <felitas@zoho.com>
2023-03-15 17:15:50 +0800

    test: qwik computed macro

---
+++ packages/qwik/tests/computed.test.ts
@@ -0,0 +1,18 @@
+import { computed, signal } from "../src"
+
+import { transform } from "@macro-plugin/core"
+
+test("qwik computed", () => {
+  const code = `
+  signal: {
+    var a = 3
+    var b = 4
+    var count = 1
+  }
+  computed: {
+    var doubleCount = 2 * count + count - a
+  }
+  `
+
+  expect(transform(code, { global: { signal, computed } }).code).toMatchSnapshot()
+})
