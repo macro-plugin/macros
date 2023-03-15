bef4389049bda38999f57ccc28ccebd049cc902c
voorjaar <felitas@zoho.com>
2023-03-15 12:03:08 +0800

    feat: add solid lifecycle macros

---
+++ packages/solid-state/src/lifecycle.ts
@@ -0,0 +1,5 @@
+import { createLabeledBlock } from "@macro-plugin/core"
+
+export const onMount = createLabeledBlock('onMount', 'solid-js');
+export const onError = createLabeledBlock('onError', 'solid-js');
+export const onCleanup = createLabeledBlock('onCleanup', 'solid-js');
