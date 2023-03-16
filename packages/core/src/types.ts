258505bee7f0a405ad22852626bf1cf56150813d
voorjaar <felitas@zoho.com>
2023-03-16 20:53:51 +0800

    refactor: add some new types for walker

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,23 +1,30 @@
-import type { BaseNode, Statement } from "estree";
+import type { CatchClause, ClassDeclaration, ClassMethod, Declaration, Expression, ImportDeclaration, Node, Options, PrivateMethod, Statement, VariableDeclarator } from "@swc/core";
 
-import type { ParserOptions } from "@babel/parser";
-
-export type {
-  LabeledStatement,
-  Statement,
-  ImportDeclaration
-} from "estree"
-
-export type { ParserOptions } from "@babel/parser"
-
-export type { BaseNode } from "estree-walker"
-
-export type ImportSpecifier = {
+export type PluginImportSpecifier = {
   name: string,
-  kind?: null | "type" | "default"
+  kind?: null | "default"
 }
 
+export type { Node } from "@swc/core"
+
 export type ScopeVar = { name: string, private?: boolean, value?: Node, marker?: unknown };
+
+export type BaseNode = Declaration | Expression | CatchClause | ClassDeclaration | ImportDeclaration | ClassMethod | PrivateMethod | Statement | VariableDeclarator;
+
+export type WalkContext = {
+  set: <T>(key: string, data: T) => void;
+  get: <T>(key: string, defaultValue?: T) => T | undefined;
+  skip: () => void;
+  remove: () => void;
+  replace: (newNode: Node | Node[]) => void;
+  import: (specifiers: PluginImportSpecifier[], source: string) => void;
+}
+
+export type WalkFunc = (this: WalkContext, node: Node, parent?: Node, prop?: string, index?: number) => Node | Node[] | undefined | void
+export type WalkPlugin = {
+  enter?: WalkFunc;
+  leave?: WalkFunc;
+}
 
 export type Handler = {
   /** Save data to current plugin cache */
@@ -31,19 +38,18 @@ export type Handler = {
   /** Replace matched node with new node, equals to `return node` */
   replace: (node: BaseNode | BaseNode[]) => void;
   /** Import some package */
-  import: (specifiers: ImportSpecifier[], source: string, kind?: 'type' | 'value' | undefined | null) => void
+  import: (specifiers: PluginImportSpecifier[], source: string) => void
 };
 
 export type TrackHandler = Handler & {
   track(name: string): ScopeVar | undefined
 }
 
-export type LabeledMacro = (ast: Statement, code: string, handler: Handler) => Statement | Statement[] | string
-export type GlobalMacro = (ast: BaseNode, handler: Handler, parent: BaseNode, prop: string, index: number) => void | BaseNode | BaseNode[]
-export type GlobalTrackMacro = (ast: BaseNode, handler: TrackHandler, parent: BaseNode, prop: string, index: number) => void | BaseNode | BaseNode[]
+export type LabeledMacro = (ast: BaseNode, code: string, handler: Handler) => BaseNode | BaseNode[] | string
+export type GlobalMacro = (ast: BaseNode, handler: Handler, parent?: BaseNode, prop?: string, index?: number) => void | BaseNode | BaseNode[]
+export type GlobalTrackMacro = (ast: BaseNode, handler: TrackHandler, parent?: BaseNode, prop?: string, index?: number) => void | BaseNode | BaseNode[]
 
-export type Config = {
+export type Config = Omit<Options, "plugin"> & {
   global?: Record<string, GlobalMacro | { enter: GlobalMacro, leave: GlobalMacro }>,
   labeled?: Record<string, LabeledMacro>,
-  parserOptions?: ParserOptions
 }
