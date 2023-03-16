34b795d8f2165ea5221869bb06ffb97dd4b4d00a
voorjaar <felitas@zoho.com>
2023-03-16 21:04:10 +0800

    refactor: from sucrase to swc

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 /** @type {import("@jest/types").Config.InitialOptions} */
 module.exports = {
-  transform: { "\\.(js|jsx|ts|tsx)$": "@sucrase/jest-plugin" },
+  transform: { "^.+\\.(t|j)sx?$": "@swc/jest" },
   moduleNameMapper: Object.fromEntries(require("fs").readdirSync("./packages").map(i => [[`^@macro-plugin/${i}`], [`<rootDir>/packages/${i}/src`]])),
   collectCoverageFrom: ["packages/**/tests/**/*.test.ts"],
 };
