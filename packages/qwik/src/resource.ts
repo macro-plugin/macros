99e2bcc6f7c291f0aab061fb1815fe110c67a307
voorjaar <felitas@zoho.com>
2023-03-16 21:53:48 +0800

    fix: switch to swc

--- packages/qwik/src/resource.ts
+++ packages/qwik/src/resource.ts
@@ -1,63 +1,106 @@
-import { Handler } from "@macro-plugin/core";
-import { Statement } from "estree";
+import { BaseNode, Handler } from "@macro-plugin/core";
+import { Identifier, Statement, VariableDeclaration } from "@swc/core";
 
 const varToReturn = (body: Statement[]) => {
-  let ident;
+  let ident: Identifier | undefined;
   body = body.map(i => {
     if (i.type == "VariableDeclaration" && i.kind == 'var') {
-      ident = i.declarations[0].id;
+      if (i.declarations[0].id.type == 'Identifier') {
+        ident = i.declarations[0].id;
+      } else {
+        throw new Error('Expect an Identifier')
+      }
       return {
         type: 'ReturnStatement',
-        argument: i.declarations[0].init
+        argument: i.declarations[0].init,
+        span: {
+          start: 0,
+          end: 0,
+          ctxt: 0
+        }
       }
     }
     return i
   })
 
-  return [ident, body]
+  return [ident, body] as [Identifier, Statement[]]
 }
 
-export const resource = (ast: Statement, code: string, handler: Handler) => {
+export const resource = (ast: BaseNode, code: string, handler: Handler) => {
   handler.import([{ name: 'useResource$' }], '@builder.io/qwik');
   if (ast.type == "BlockStatement") {
-    const [id, body] = varToReturn(ast.body);
+    const [id, stmts] = varToReturn(ast.stmts);
 
     return {
       type: "VariableDeclaration",
       declarations: [
         {
           type: "VariableDeclarator",
+          definite: false,
+          span: {
+            start: 0,
+            end: 0,
+            ctxt: 0
+          },
           id,
           init: {
             type: 'CallExpression',
             callee: {
               type: 'Identifier',
-              name: 'useResource$'
+              optional: false,
+              value: 'useResource$',
+              span: {
+                start: 0,
+                end: 0,
+                ctxt: 0
+              }
+            },
+            span: {
+              start: 0,
+              end: 0,
+              ctxt: 0
             },
             arguments: [
-              // @ts-ignore
               {
-                type: 'ArrowFunctionExpression',
-                generator: false,
-                async: false,
-                params: [],
-                body: {
-                  type: 'BlockStatement',
-                  body
+                expression: {
+                  type: 'ArrowFunctionExpression',
+                  generator: false,
+                  async: false,
+                  params: [],
+                  span: {
+                    start: 0,
+                    end: 0,
+                    ctxt: 0
+                  },
+                  body: {
+                    type: 'BlockStatement',
+                    stmts,
+                    span: {
+                      start: 0,
+                      end: 0,
+                      ctxt: 0
+                    },
+                  }
                 }
               }
             ]
           }
         }
       ],
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0
+      },
+      declare: false,
       kind: "const"
-    } as unknown as Statement
+    } as VariableDeclaration
   } else if (ast.type == "ExpressionStatement" && ast.expression.type == 'ArrowFunctionExpression') {
-    let id, body;
+    let id, stmts;
 
     if (ast.expression.body.type == 'BlockStatement') {
-      [id, body] = varToReturn(ast.expression.body.body);
-      ast.expression.body.body = body!;
+      [id, stmts] = varToReturn(ast.expression.body.stmts);
+      ast.expression.body.stmts = stmts;
     } else {
       throw new Error('expect an arrow block inside resource.')
     }
@@ -67,21 +110,46 @@ export const resource = (ast: Statement, code: string, handler: Handler) => {
       declarations: [
         {
           type: "VariableDeclarator",
+          definite: false,
+          span: {
+            start: 0,
+            end: 0,
+            ctxt: 0
+          },
           id,
           init: {
             type: 'CallExpression',
             callee: {
               type: 'Identifier',
-              name: 'useResource$'
+              value: 'useResource$',
+              span: {
+                start: 0,
+                end: 0,
+                ctxt: 0
+              },
+              optional: false
             },
             arguments: [
-              ast.expression
-            ]
+              {
+                expression: ast.expression
+              }
+            ],
+            span: {
+              start: 0,
+              end: 0,
+              ctxt: 0
+            },
           }
         }
       ],
-      kind: "const"
-    } as unknown as Statement
+      kind: "const",
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0
+      },
+      declare: false,
+    } as VariableDeclaration
   } else {
     throw new Error('this macro only accept a ArrowFunction or BlockStatement')
   }
