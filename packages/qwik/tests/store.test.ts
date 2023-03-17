9fc744ef9433af544f9e44b0d4b2ccfe535c4e9b
voorjaar <felitas@zoho.com>
2023-03-18 02:09:11 +0800

    test: store macro

---
+++ packages/qwik/tests/store.test.ts
@@ -0,0 +1,24 @@
+import { qwik, store } from "../src"
+
+import { transform } from "@macro-plugin/core"
+
+test("qwik store", () => {
+  const code = `
+  export default () => {
+    qwik: true
+    store: {
+      var state = {
+        count: 0
+      }
+    }
+
+    return (
+      <>
+        <button onClick$={() => state.count++}>Increment</button>
+        Count: {state.count}
+      </>
+    );
+  };
+  `
+  expect(transform(code, {plugins: [qwik, store], jsc: { parser: { syntax: 'typescript', tsx: true } }}).code).toMatchSnapshot();
+})
