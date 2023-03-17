00d689a5f5c7a637ee35dc85d6756814d9a24229
voorjaar <felitas@zoho.com>
2023-03-17 23:24:06 +0800

    refactor: switch to new plugin api

--- packages/core/src/block.ts
+++ packages/core/src/block.ts
@@ -1,5 +1,7 @@
 import { ArrowFunctionExpression, ExpressionStatement } from "@swc/core";
-import { Handler, LabeledMacro } from "./types";
+
+import { MacroPlugin } from "./types";
+import { createPlugin } from "./transform";
 
 /**
  * Create a macro plugin that converts `labeled: {}` or `labeled: (...args) => {}` to `$specifier((...args) => {})`,
@@ -9,111 +11,114 @@ import { Handler, LabeledMacro } from "./types";
  * @param allowParams - allow convert the input array to params, default is false.
  * @returns - A labeled macro plugin
  */
-export const createLabeledBlock: ((specifier: string, source: string, allowParams?: boolean) => LabeledMacro) = (specifier, source, allowParams = false) => {
-  return (ast, code: string, handler: Handler) => {
-    handler.import([{ name: specifier }], source);
-    if (ast.type == "BlockStatement") {
-      return {
-        type: "ExpressionStatement",
-        expression: {
-          type: 'CallExpression',
-          callee: {
-            type: 'Identifier',
-            value: specifier,
+export const createLabeledBlock: ((label: string, specifier: string, source: string, allowParams?: boolean) => MacroPlugin) = (label, specifier, source, allowParams = false) => {
+  return createPlugin({
+    LabeledStatement(ast) {
+      if (ast.label.value != label) return;
+      this.import([{ name: specifier }], source);
+      if (ast.body.type == "BlockStatement") {
+        return ({
+          type: "ExpressionStatement",
+          expression: {
+            type: 'CallExpression',
+            callee: {
+              type: 'Identifier',
+              value: specifier,
+              span: {
+                start: 0,
+                end: 0,
+                ctxt: 0
+              }
+            },
+            arguments: [
+              {
+                "expression": {
+                  type: 'ArrowFunctionExpression',
+                  generator: false,
+                  async: false,
+                  params: [],
+                  body: ast.body,
+                  span: {
+                    start: 0,
+                    end: 0,
+                    ctxt: 0
+                  }
+                } as ArrowFunctionExpression
+              }
+            ],
             span: {
               start: 0,
               end: 0,
               ctxt: 0
             }
           },
-          arguments: [
-            {
-              "expression": {
-                type: 'ArrowFunctionExpression',
-                generator: false,
-                async: false,
-                params: [],
-                body: ast,
-                span: {
-                  start: 0,
-                  end: 0,
-                  ctxt: 0
-                }
-              } as ArrowFunctionExpression
-            }
-          ],
           span: {
             start: 0,
             end: 0,
             ctxt: 0
           }
-        },
-        span: {
-          start: 0,
-          end: 0,
-          ctxt: 0
-        }
-      } as ExpressionStatement
-    } else if (ast.type == "ExpressionStatement" && ast.expression.type == 'ArrowFunctionExpression') {
-      return {
-        type: "ExpressionStatement",
-        expression: {
-          type: 'CallExpression',
-          callee: {
-            type: 'Identifier',
-            value: specifier,
+        }) as ExpressionStatement
+      } else if (ast.body.type == "ExpressionStatement" && ast.body.expression.type == 'ArrowFunctionExpression') {
+        return ({
+          type: "ExpressionStatement",
+          expression: {
+            type: 'CallExpression',
+            callee: {
+              type: 'Identifier',
+              value: specifier,
+              span: {
+                start: 0,
+                end: 0,
+                ctxt: 0
+              }
+            },
+            arguments: [
+              {
+                expression: ast.body.expression
+              }
+            ],
             span: {
               start: 0,
               end: 0,
               ctxt: 0
             }
           },
-          arguments: [
-            {
-              expression: ast.expression
-            }
-          ],
           span: {
             start: 0,
             end: 0,
             ctxt: 0
           }
-        },
-        span: {
-          start: 0,
-          end: 0,
-          ctxt: 0
-        }
-      } as ExpressionStatement
-    } else if (allowParams && ast.type == "ExpressionStatement" && ast.expression.type == 'ArrayExpression') {
-      return {
-        type: "ExpressionStatement",
-        expression: {
-          type: 'CallExpression',
-          callee: {
-            type: 'Identifier',
-            value: specifier,
+        }) as ExpressionStatement
+      } else if (allowParams && ast.body.type == "ExpressionStatement" && ast.body.expression.type == 'ArrayExpression') {
+        return ({
+          type: "ExpressionStatement",
+          expression: {
+            type: 'CallExpression',
+            callee: {
+              type: 'Identifier',
+              value: specifier,
+              span: {
+                start: 0,
+                end: 0,
+                ctxt: 0
+              }
+            },
+            arguments: ast.body.expression.elements,
             span: {
               start: 0,
               end: 0,
               ctxt: 0
             }
           },
-          arguments: ast.expression.elements,
           span: {
             start: 0,
             end: 0,
             ctxt: 0
           }
-        },
-        span: {
-          start: 0,
-          end: 0,
-          ctxt: 0
-        }
-      } as ExpressionStatement
-    } else {
-      throw new Error('this macro only accept a ArrowFunction or BlockStatement' + (allowParams ? ' or an ArrayExpression' : ''))
+        }) as ExpressionStatement
+      } else {
+        throw new Error('this macro only accept a ArrowFunction or BlockStatement' + (allowParams ? ' or an ArrayExpression' : ''))
+      }
     }
-  }
+  })
 }
