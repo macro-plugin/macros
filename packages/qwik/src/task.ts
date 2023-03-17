bc44d7f7dcc6a71cc9cf196f677f8517a5076761
voorjaar <felitas@zoho.com>
2023-03-17 23:30:48 +0800

    refactor: use new plugin api

--- packages/qwik/src/task.ts
+++ packages/qwik/src/task.ts
@@ -1,4 +1,4 @@
 import { createLabeledBlock } from "@macro-plugin/core";
 
-export const task = createLabeledBlock('useTask$', '@builder.io/qwik');
-export const vtask = createLabeledBlock('useVisibleTask$', '@builder.io/qwik', true);
+export const task = createLabeledBlock('task', 'useTask$', '@builder.io/qwik');
+export const vtask = createLabeledBlock('vtask', 'useVisibleTask$', '@builder.io/qwik', true);
