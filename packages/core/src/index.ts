758563fb81ae83b5bdb750790c0736e7a86d6dbf
voorjaar <felitas@zoho.com>
2023-03-18 13:15:51 +0800

    fix: api exports

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,8 @@
-import { createLabeledBlock } from "./block";
+export { createLabeledBlock } from "./block";
+export { createLabeledExpr } from "./expr";
 export { transform, createPlugin } from "./transform";
 export { generate } from "./generate";
 export { parse } from "./parse";
 export { walk } from "./walk";
-export { createLabeledBlock };
 export * from "./types";
 export * from "./utils";
