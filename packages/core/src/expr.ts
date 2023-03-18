82136ee8e722d216c8c722ab5b7534efe4059322
voorjaar <felitas@zoho.com>
2023-03-18 13:15:34 +0800

    feat: new createLabeledExpr api

---
+++ packages/core/src/expr.ts
@@ -0,0 +1,42 @@
+import { CallExpression } from "@swc/core";
+import { MacroPlugin } from "./types";
+import { createPlugin } from "./transform";
+
+export const createLabeledExpr: ((label: string, specifier: string, source: string) => MacroPlugin) = (label, specifier, source) => {
+  return createPlugin({
+    LabeledStatement(ast) {
+      if (ast.label.value != label) return;
+      if (ast.body.type == "ExpressionStatement") {
+        this.import([{ name: specifier }], source);
+
+        ast.body.expression = {
+          "type": "CallExpression",
+          "span": {
+            "start": 109,
+            "end": 119,
+            "ctxt": 0
+          },
+          "callee": {
+            "type": "Identifier",
+            "span": {
+              "start": 109,
+              "end": 112,
+              "ctxt": 1
+            },
+            "value": specifier,
+            "optional": false
+          },
+          "arguments": [
+            {
+              "expression": ast.body.expression
+            }
+          ],
+        } as CallExpression
+
+        return ast.body
+      } else {
+        throw new Error('this macro only accept an Expression')
+      }
+    }
+  })
+}
