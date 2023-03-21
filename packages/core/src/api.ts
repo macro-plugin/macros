232db851bf421d2a4596fa484db7ef6d7d42d596
voorjaar <felitas@zoho.com>
2023-03-21 22:17:27 +0800

    feat: new createLitMacro api

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -1,11 +1,34 @@
 import type { BaseNode, LabeledMacro, MacroPlugin } from "./types";
 
+import { isRegExp } from "./utils";
+
 export function createMacro(macro: MacroPlugin) {
   return macro;
 }
 
 export function createLitMacro<T>(key: string, value: T) {
-
+  return createMacro({
+    Identifier(ast) {
+      if (ast.value == key && !this.track(key)) {
+        if (value === undefined) return {
+          "type": "Identifier",
+          "span": {
+            "start": 0,
+            "end": 0,
+            "ctxt": 2
+          },
+          "value": "undefined",
+          "optional": false
+        }
+        if (value && typeof value == 'object') {
+          if (isRegExp(value)) return this.parseExpr(value.toString())
+          if ('span' in value && 'type' in value) return value as BaseNode;
+        }
+        if (typeof value == 'function') return this.parseExpr(value.toString())
+        return this.parseExpr(JSON.stringify(value))
+      }
+    }
+  })
 }
 
 export function createExprMacro() {
