199d6b35a8576f47cc3169023fd47b091c047873
voorjaar <felitas@zoho.com>
2023-03-21 20:49:24 +0800

    refactor: use new createLabeledMacro api

--- packages/core/src/block.ts
+++ packages/core/src/block.ts
@@ -1,7 +1,7 @@
 import { ArrowFunctionExpression, ExpressionStatement } from "@swc/core";
 
 import { MacroPlugin } from "./types";
-import { createMacro } from "./api";
+import { createLabeledMacro } from "./api";
 
 /**
  * Create a macro plugin that converts `labeled: {}` or `labeled: (...args) => {}` to `$specifier((...args) => {})`,
@@ -11,114 +11,109 @@ import { createMacro } from "./api";
  * @param allowParams - allow convert the input array to params, default is false.
  * @returns - A labeled macro plugin
  */
-export const createLabeledBlock: ((label: string, specifier: string, source: string, allowParams?: boolean) => MacroPlugin) = (label, specifier, source, allowParams = false) => {
-  return createMacro({
-    LabeledStatement(ast) {
-      if (ast.label.value != label) return;
-      this.import([{ name: specifier }], source);
-      if (ast.body.type == "BlockStatement") {
-        return ({
-          type: "ExpressionStatement",
-          expression: {
-            type: 'CallExpression',
-            callee: {
-              type: 'Identifier',
-              value: specifier,
-              span: {
-                start: 0,
-                end: 0,
-                ctxt: 0
-              }
-            },
-            arguments: [
-              {
-                "expression": {
-                  type: 'ArrowFunctionExpression',
-                  generator: false,
-                  async: false,
-                  params: [],
-                  body: ast.body,
-                  span: {
-                    start: 0,
-                    end: 0,
-                    ctxt: 0
-                  }
-                } as ArrowFunctionExpression
-              }
-            ],
-            span: {
-              start: 0,
-              end: 0,
-              ctxt: 0
-            }
-          },
+export const createLabeledBlock: ((label: string, specifier: string, source: string, allowParams?: boolean) => MacroPlugin) = (label, specifier, source, allowParams = false) => createLabeledMacro(label, function (ast) {
+  this.import([{ name: specifier }], source);
+  if (ast.type == "BlockStatement") {
+    return ({
+      type: "ExpressionStatement",
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          value: specifier,
           span: {
             start: 0,
             end: 0,
             ctxt: 0
           }
-        }) as ExpressionStatement
-      } else if (ast.body.type == "ExpressionStatement" && ast.body.expression.type == 'ArrowFunctionExpression') {
-        return ({
-          type: "ExpressionStatement",
-          expression: {
-            type: 'CallExpression',
-            callee: {
-              type: 'Identifier',
-              value: specifier,
+        },
+        arguments: [
+          {
+            "expression": {
+              type: 'ArrowFunctionExpression',
+              generator: false,
+              async: false,
+              params: [],
+              body: ast,
               span: {
                 start: 0,
                 end: 0,
                 ctxt: 0
               }
-            },
-            arguments: [
-              {
-                expression: ast.body.expression
-              }
-            ],
-            span: {
-              start: 0,
-              end: 0,
-              ctxt: 0
-            }
-          },
-          span: {
-            start: 0,
-            end: 0,
-            ctxt: 0
+            } as ArrowFunctionExpression
           }
-        }) as ExpressionStatement
-      } else if (allowParams && ast.body.type == "ExpressionStatement" && ast.body.expression.type == 'ArrayExpression') {
-        return ({
-          type: "ExpressionStatement",
-          expression: {
-            type: 'CallExpression',
-            callee: {
-              type: 'Identifier',
-              value: specifier,
-              span: {
-                start: 0,
-                end: 0,
-                ctxt: 0
-              }
-            },
-            arguments: ast.body.expression.elements,
-            span: {
-              start: 0,
-              end: 0,
-              ctxt: 0
-            }
-          },
-          span: {
-            start: 0,
-            end: 0,
-            ctxt: 0
-          }
-        }) as ExpressionStatement
-      } else {
-        throw new Error('this macro only accept a ArrowFunction or BlockStatement' + (allowParams ? ' or an ArrayExpression' : ''))
+        ],
+        span: {
+          start: 0,
+          end: 0,
+          ctxt: 0
+        }
+      },
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0
       }
-    }
-  })
-}
+    }) as ExpressionStatement
+  } else if (ast.type == "ExpressionStatement" && ast.expression.type == 'ArrowFunctionExpression') {
+    return ({
+      type: "ExpressionStatement",
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          value: specifier,
+          span: {
+            start: 0,
+            end: 0,
+            ctxt: 0
+          }
+        },
+        arguments: [
+          {
+            expression: ast.expression
+          }
+        ],
+        span: {
+          start: 0,
+          end: 0,
+          ctxt: 0
+        }
+      },
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0
+      }
+    }) as ExpressionStatement
+  } else if (allowParams && ast.type == "ExpressionStatement" && ast.expression.type == 'ArrayExpression') {
+    return ({
+      type: "ExpressionStatement",
+      expression: {
+        type: 'CallExpression',
+        callee: {
+          type: 'Identifier',
+          value: specifier,
+          span: {
+            start: 0,
+            end: 0,
+            ctxt: 0
+          }
+        },
+        arguments: ast.expression.elements,
+        span: {
+          start: 0,
+          end: 0,
+          ctxt: 0
+        }
+      },
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0
+      }
+    }) as ExpressionStatement
+  } else {
+    throw new Error('this macro only accept a ArrowFunction or BlockStatement' + (allowParams ? ' or an ArrayExpression' : ''))
+  }
+})
