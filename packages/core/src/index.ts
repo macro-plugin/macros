e2a3639a84c11225188ea509a9f6a0a0507ccbb5
voorjaar <felitas@zoho.com>
2023-03-21 22:33:07 +0800

    feat: new parseExpr and parseExprAsync api

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,7 +2,7 @@ export { createLabeledBlock } from "./block";
 export { createLabeledExpr } from "./expr";
 export { transform, transformAst, transformAsync } from "./transform";
 export { generate } from "./generate";
-export { parse } from "./parse";
+export { parse, parseAsync, parseExpr, parseExprAsync } from "./parse";
 export { walk } from "./walk";
 export * from "./api";
 export * from "./types";
--- packages/core/src/parse.ts
+++ packages/core/src/parse.ts
@@ -1,1 +1,25 @@
+import type { Expression, ModuleItem, ParseOptions } from "@swc/core";
+import { parse, parseSync } from "@swc/core"
+
 export { parse as parseAsync, parseSync as parse } from "@swc/core"
+
+function extractExpr(stmt: ModuleItem | undefined): Expression {
+  if (stmt && stmt.type == 'ExpressionStatement' && stmt.expression.type == 'ParenthesisExpression') return stmt.expression.expression;
+  return {
+    type: "Invalid",
+    span: {
+      start: 0,
+      end: 0,
+      ctxt: 0
+    }
+  }
+}
+
+export function parseExpr(expr: string, options?: ParseOptions): Expression {
+  return extractExpr(parseSync('(' + expr + ')', options).body[0])
+}
+
+export async function parseExprAsync(expr: string, options: ParseOptions) {
+  const stmt = (await parse('(' + expr + ')', options)).body[0]
+  return extractExpr(stmt);
+}
