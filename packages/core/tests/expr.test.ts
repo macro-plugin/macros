11e9437547916447330ba65c6ad2c59b035275ac
voorjaar <felitas@zoho.com>
2023-03-22 21:08:30 +0800

    test: type macro and expr macro

--- packages/core/tests/expr.test.ts
+++ packages/core/tests/expr.test.ts
@@ -1,49 +1,147 @@
-import { createExprMacro } from "../src"
-import { transform } from "../src"
+import { createExprMacro, createTypeMacro, transform } from "../src"
 
-test("create expr macro", () => {
-  const code = `
-  $add(1, 2)
-  `
-
-  const macro = createExprMacro('$add', function($a: number, $b: number) {
-    return $a + $b
-  })
-
-  expect(transform(code, { plugins: [macro] }).code).toBe('1 + 2;\n')
-})
+import { StringLiteral } from "@swc/core"
 
 test("create expr macro with arrow function", () => {
-  const code = `
-  $add(1, 2)
-  `
-
-  const macro = createExprMacro('$add', ($a: number, $b: number) => {
+  const macro = createExprMacro("$add", ($a: number, $b: number) => {
     return $a + $b
   })
 
-  expect(transform(code, { plugins: [macro] }).code).toBe('1 + 2;\n')
-});
+  expect(transform("$add(1, 2)", { plugins: [macro] }).code).toBe("1 + 2;\n")
+})
 
 test("create expr macro with arrow expr", () => {
-  const code = `
-  $add(1, 2)
-  `
+  const macro = createExprMacro("$add", ($a: number, $b: number) => $a + $b)
 
-  const macro = createExprMacro('$add', ($a: number, $b: number) => $a + $b)
-
-  expect(transform(code, { plugins: [macro] }).code).toBe('1 + 2;\n')
-});
+  expect(transform("$add(1, 2)", { plugins: [macro] }).code).toBe("1 + 2;\n")
+})
 
 test("expr macro with condition", () => {
-  const code = `
-  $add(-1, 2)
-  `
+  const macro = createExprMacro("$add", ($a: number, $b: number) => $a < 0 ? $b : $a + $b)
+  expect(transform("$add(-1, 2)", { plugins: [macro] }).code).toBe("-1 < 0 ? 2 : -1 + 2;\n")
+})
 
-  const macro = createExprMacro('$add', ($a: number, $b: number) => {
-    if ($a < 0) return $b;
-    return $a + $b;
+test("create complex expr macro", () => {
+  const macro = createExprMacro("$add", function (args) {
+    const a = this.printExpr(args[0])
+    const b = this.printExpr(args[1])
+
+    if (+a < 0) return args[1]
+    return this.parseExpr(`${a} + ${b}`)
   })
 
-  expect(transform(code, { plugins: [macro] }).code).toBe('2;\n')
+  expect(transform("$add(-1, 2)", { plugins: [macro] }).code).toBe("2;\n")
+  expect(transform("$add(1, 2)", { plugins: [macro] }).code).toBe("1 + 2;\n")
+})
+
+test("create a type macro with createExprMacro", () => {
+  const macro = createExprMacro("defineEmits", function (args, typeParams) {
+    const emits: StringLiteral[] = []
+    for (const t of typeParams ?? []) {
+      if (t.type === "TsTypeLiteral") {
+        for (const m of t.members) {
+          if (m.type === "TsCallSignatureDeclaration") {
+            for (const p of m.params) {
+              if (p.type === "Identifier" && p.value === "e" && p.typeAnnotation?.type === "TsTypeAnnotation" && p.typeAnnotation.typeAnnotation.type === "TsLiteralType" && p.typeAnnotation.typeAnnotation.literal.type === "StringLiteral") {
+                emits.push(p.typeAnnotation.typeAnnotation.literal)
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return {
+      type: "CallExpression",
+      span: {
+        start: 0,
+        end: 23,
+        ctxt: 0
+      },
+      callee: {
+        type: "Identifier",
+        span: {
+          start: 0,
+          end: 11,
+          ctxt: 1
+        },
+        value: "defineEmits",
+        optional: false
+      },
+      arguments: [
+        {
+          expression: {
+            type: "ArrayExpression",
+            span: {
+              start: 12,
+              end: 22,
+              ctxt: 0
+            },
+            elements: emits.map(i => ({ expression: i }))
+          }
+        }
+      ],
+    }
+  })
+
+  expect(transform(`const emit = defineEmits<{
+    (e: 'change', id: number): void
+    (e: 'update', value: string): void
+  }>()`, { plugins: [macro], jsc: { parser: { syntax: "typescript" } } }).code).toMatchSnapshot()
+})
+
+test("create a type macro with createTypeMacro", () => {
+  const macro = createTypeMacro("defineEmits", function (typeParams) {
+    const emits: StringLiteral[] = []
+    for (const t of typeParams ?? []) {
+      if (t.type === "TsTypeLiteral") {
+        for (const m of t.members) {
+          if (m.type === "TsCallSignatureDeclaration") {
+            for (const p of m.params) {
+              if (p.type === "Identifier" && p.value === "e" && p.typeAnnotation?.type === "TsTypeAnnotation" && p.typeAnnotation.typeAnnotation.type === "TsLiteralType" && p.typeAnnotation.typeAnnotation.literal.type === "StringLiteral") {
+                emits.push(p.typeAnnotation.typeAnnotation.literal)
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return {
+      type: "CallExpression",
+      span: {
+        start: 0,
+        end: 23,
+        ctxt: 0
+      },
+      callee: {
+        type: "Identifier",
+        span: {
+          start: 0,
+          end: 11,
+          ctxt: 1
+        },
+        value: "defineEmits",
+        optional: false
+      },
+      arguments: [
+        {
+          expression: {
+            type: "ArrayExpression",
+            span: {
+              start: 12,
+              end: 22,
+              ctxt: 0
+            },
+            elements: emits.map(i => ({ expression: i }))
+          }
+        }
+      ],
+    }
+  })
+
+  expect(transform(`const emit = defineEmits<{
+    (e: 'change', id: number): void
+    (e: 'update', value: string): void
+  }>()`, { plugins: [macro], jsc: { parser: { syntax: "typescript" } } }).code).toMatchSnapshot()
 })
