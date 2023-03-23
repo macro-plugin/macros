3ba39c9452f6cb659e4960976081dd8566a4be79
voorjaar <felitas@zoho.com>
2023-03-24 03:30:06 +0800

    test: complex macros

--- packages/core/tests/macro.test.ts
+++ packages/core/tests/macro.test.ts
@@ -44,3 +44,80 @@ let c = $add(1, 2)
 
   expect(transform(code, { plugins: [macro], jsc: { parser: { syntax: "typescript" } } }).code).toMatchSnapshot()
 })
+
+test("create complex macro with macros", () => {
+  const code = `
+macro: {
+  var __num__ = $Lit<number>(4)
+
+  var add = $Expr<(a: number, b: number) => number>(function(args) {
+    const a = this.printExpr(args[0])
+    const b = this.printExpr(args[1])
+
+    if (+a < 0) return args[1]
+    return this.parseExpr(\`\${a} + \${b}\`)
+  })
+
+  var typeAdd = $Type<(<a extends number, b extends number>() => number)>(function (typeParams) {
+    return this.parseExpr((typeParams[0].literal.value + typeParams[1].literal.value).toString())
+  })
+
+  var real = $Tmpl(function (strings, personExpr, ageExpr) {
+    return {
+      type: "StringLiteral",
+      span: {
+        start: 0,
+        end: 0,
+        ctxt: 0,
+      },
+      value: strings[0] + "Bob" + strings[1] + "12" + strings[2]
+    }
+  })
+
+  $Labeled("debug", function (stmt) {
+    return {
+      "type": "IfStatement",
+      "span": {
+        "start": 0,
+        "end": 28,
+        "ctxt": 0
+      },
+      "test": {
+        "type": "Identifier",
+        "span": {
+          "start": 4,
+          "end": 13,
+          "ctxt": 1
+        },
+        "value": "__debug__",
+        "optional": false
+      },
+      "consequent": {
+        "type": "BlockStatement",
+        "span": {
+          "start": 15,
+          "end": 28,
+          "ctxt": 0
+        },
+        "stmts": [stmt]
+      }
+    }
+  })
+
+  $Macro({
+    StringLiteral(ast) {
+      if (ast.value === 'hello') ast.value = 'world'
+    }
+  })
+}
+
+let a = __num__
+let b = add(1, 2)
+let c = typeAdd<3, 5>()
+let d = real\`That \${person} is \${age}\`
+debug: console.log('debug')
+let f = 'hello'
+`
+
+  expect(transform(code, { plugins: [macro], jsc: { parser: { syntax: "typescript" } } }).code).toMatchSnapshot()
+})
