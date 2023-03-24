9cd4b7dfc2b7f1e9421bd940b4fa3b4c4305fbde
voorjaar <felitas@zoho.com>
2023-03-24 23:33:33 +0800

    fix: typos

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,4 @@ module.exports = {
   transform: { "^.+\\.(t|j)sx?$": "@swc/jest" },
   moduleNameMapper: Object.fromEntries(require("fs").readdirSync("./packages").map(i => [[`^@macro-plugin/${i}`], [`<rootDir>/packages/${i}/src`]])),
   collectCoverageFrom: ["packages/**/tests/**/*.test.ts"],
-};
+}
