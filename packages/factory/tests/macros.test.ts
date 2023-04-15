cbd54dbba2e85f6d890b4d64a50e2b5fe963b03a
voorjaar <felitas@zoho.com>
2023-04-16 04:07:06 +0800

    test: macro factorys

---
+++ packages/factory/tests/macros.test.ts
@@ -0,0 +1,47 @@
+import { $Argument, $CallExpression, $Identifier, $StringLiteral } from "../src"
+
+import { transform } from "@macro-plugin/core"
+
+test("$Identifier macro transform", () => {
+  const code = `
+    function id() {
+      const a = $Identifier("value")
+      const b = $Identifier("value", false)
+      const c = $Identifier("value", true)
+      const d = "value"
+      const e = $Identifier(d, true)
+    }
+  `
+
+  expect(transform(code, { plugins: [$Identifier] }).code).toMatchSnapshot()
+})
+
+test("$StringLiteral macro transform", () => {
+  const code = `
+  const a = $StringLiteral("hello")
+  const v = "world"
+  const b = $StringLiteral(v)
+  `
+
+  expect(transform(code, { plugins: [$StringLiteral] }).code).toMatchSnapshot()
+})
+
+test("$Argument macro transform", () => {
+  const code = `
+  const a = $Argument($Identifier("ref"))
+  const b = $Argument($Identifier("list"), true)
+`
+
+  expect(transform(code, { plugins: [$Identifier, $Argument] }).code).toMatchSnapshot()
+})
+
+test("$CallExpression macro transform", () => {
+  const code = `
+  const a = $CallExpression($Identifier("ref"))
+  const b = $CallExpression($Identifier("ref"), [
+    $Argument($StringLiteral("hello")),
+  ])
+`
+
+  expect(transform(code, { plugins: [$Identifier, $CallExpression, $Argument, $StringLiteral] }).code).toMatchSnapshot()
+})
