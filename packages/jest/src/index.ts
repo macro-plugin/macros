991e340b3e6acb47781f33188b772a9c9cb7f0bd
voorjaar <felitas@zoho.com>
2023-04-16 05:29:01 +0800

    refactor: separate code

--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -1,92 +1,11 @@
 import { Config, createSwcPlugin } from "@macro-plugin/core"
-import { Options, version as swcVersion, transform, transformSync } from "@swc/core"
 import type { TransformOptions, Transformer } from "@jest/transform"
+import { buildTransformOpts, insertInstrumentationOptions } from "./utils"
+import { version as swcVersion, transform, transformSync } from "@swc/core"
 
 import { createHash } from "crypto"
-import { existsSync } from "fs"
 import getCacheKeyFunction from "@jest/create-cache-key-function"
-import path from "path"
-import process from "process"
 import { version } from "../package.json"
-
-const nodeTargetDefaults = new Map([
-  ["12", "es2018"],
-  ["13", "es2019"],
-  ["14", "es2020"],
-  ["15", "es2021"],
-  ["16", "es2021"],
-  ["17", "es2022"],
-])
-
-function set (obj: any, path: string, value: any) {
-  let o = obj
-  const parents = path.split(".")
-  const key = parents.pop() as string
-
-  for (const prop of parents) {
-    if (o[prop] == null) o[prop] = {}
-    o = o[prop]
-  }
-
-  o[key] = value
-}
-
-function loadConfigFile (): Options {
-  const configFile = path.join(process.cwd(), "macros.config.js")
-  if (existsSync(configFile)) {
-    const options = require(configFile)
-    return options as Options
-  }
-  return {}
-}
-
-function buildTransformOpts (swcOptions: (Config & { experimental?: unknown }) | undefined): Options {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { experimental, ...computedSwcOptions } = swcOptions || (loadConfigFile() as Options & { experimental?: unknown })
-
-  if (!computedSwcOptions.jsc?.target) {
-    set(
-      computedSwcOptions,
-      "jsc.target",
-      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-      nodeTargetDefaults.get(process.version.match(/v(\d+)/)![1]) || "es2018"
-    )
-  }
-
-  set(computedSwcOptions, "jsc.transform.hidden.jest", true)
-
-  if (!computedSwcOptions.sourceMaps) {
-    set(computedSwcOptions, "sourceMaps", "inline")
-  }
-
-  return computedSwcOptions
-}
-
-function insertInstrumentationOptions (jestOptions: TransformOptions<unknown>, canInstrument: boolean, swcTransformOpts: Options, instrumentOptions?: any) {
-  const shouldInstrument = jestOptions.instrument && canInstrument
-
-  if (!shouldInstrument) {
-    return swcTransformOpts
-  }
-
-  if (swcTransformOpts?.jsc?.experimental?.plugins?.some((x) => x[0] === "swc-plugin-coverage-instrument")) {
-    return
-  }
-
-  if (!swcTransformOpts.jsc) {
-    swcTransformOpts.jsc = {}
-  }
-
-  if (!swcTransformOpts.jsc.experimental) {
-    swcTransformOpts.jsc.experimental = {}
-  }
-
-  if (!Array.isArray(swcTransformOpts.jsc.experimental.plugins)) {
-    swcTransformOpts.jsc.experimental.plugins = []
-  }
-
-  swcTransformOpts.jsc.experimental.plugins?.push(["swc-plugin-coverage-instrument", instrumentOptions ?? {}])
-}
 
 function createTransformer (swcTransformOpts: Config & {
   experimental?: {
---
+++ packages/jest/src/utils.ts
@@ -0,0 +1,85 @@
+import type { Config } from "@macro-plugin/core"
+import type { Options } from "@swc/core"
+import type { TransformOptions } from "@jest/transform"
+import { existsSync } from "fs"
+import path from "path"
+import process from "process"
+
+const nodeTargetDefaults = new Map([
+  ["12", "es2018"],
+  ["13", "es2019"],
+  ["14", "es2020"],
+  ["15", "es2021"],
+  ["16", "es2021"],
+  ["17", "es2022"],
+])
+
+function set (obj: any, path: string, value: any) {
+  let o = obj
+  const parents = path.split(".")
+  const key = parents.pop() as string
+
+  for (const prop of parents) {
+    if (o[prop] == null) o[prop] = {}
+    o = o[prop]
+  }
+
+  o[key] = value
+}
+
+function loadConfigFile (): Options {
+  const configFile = path.join(process.cwd(), "macros.config.js")
+  if (existsSync(configFile)) {
+    const options = require(configFile)
+    return options as Options
+  }
+  return {}
+}
+
+export function buildTransformOpts (swcOptions: (Config & { experimental?: unknown }) | undefined): Options {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { experimental, ...computedSwcOptions } = swcOptions || (loadConfigFile() as Options & { experimental?: unknown })
+
+  if (!computedSwcOptions.jsc?.target) {
+    set(
+      computedSwcOptions,
+      "jsc.target",
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      nodeTargetDefaults.get(process.version.match(/v(\d+)/)![1]) || "es2018"
+    )
+  }
+
+  set(computedSwcOptions, "jsc.transform.hidden.jest", true)
+
+  if (!computedSwcOptions.sourceMaps) {
+    set(computedSwcOptions, "sourceMaps", "inline")
+  }
+
+  return computedSwcOptions
+}
+
+export function insertInstrumentationOptions (jestOptions: TransformOptions<unknown>, canInstrument: boolean, swcTransformOpts: Options, instrumentOptions?: any) {
+  const shouldInstrument = jestOptions.instrument && canInstrument
+
+  if (!shouldInstrument) {
+    return swcTransformOpts
+  }
+
+  if (swcTransformOpts?.jsc?.experimental?.plugins?.some((x) => x[0] === "swc-plugin-coverage-instrument")) {
+    return
+  }
+
+  if (!swcTransformOpts.jsc) {
+    swcTransformOpts.jsc = {}
+  }
+
+  if (!swcTransformOpts.jsc.experimental) {
+    swcTransformOpts.jsc.experimental = {}
+  }
+
+  if (!Array.isArray(swcTransformOpts.jsc.experimental.plugins)) {
+    swcTransformOpts.jsc.experimental.plugins = []
+  }
+
+  swcTransformOpts.jsc.experimental.plugins?.push(["swc-plugin-coverage-instrument", instrumentOptions ?? {}])
+}
