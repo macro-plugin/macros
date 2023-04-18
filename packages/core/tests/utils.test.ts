6d3cc976df7fec2184437a5521c683885cf4eeb7
voorjaar <felitas@zoho.com>
2023-04-19 05:26:23 +0800

    test: macro plugin and proxy check

---
+++ packages/core/tests/utils.test.ts
@@ -0,0 +1,17 @@
+import { $Ast, $Column, $Eval, $Quote, isMacroPlugin, isMacroProxy } from "../src"
+
+test("isMacroPlugin", () => {
+  expect(isMacroPlugin($Ast)).toBeTruthy()
+  expect(isMacroPlugin($Column)).toBeTruthy()
+  expect(isMacroPlugin($Eval)).toBeTruthy()
+  expect(isMacroPlugin($Quote)).toBeTruthy()
+})
+
+test("isMacroProxy", () => {
+  expect(isMacroProxy($Ast)).toBeFalsy()
+  expect(isMacroProxy($Column)).toBeFalsy()
+  expect(isMacroProxy($Eval)).toBeFalsy()
+  expect(isMacroProxy($Quote)).toBeFalsy()
+
+  expect($Ast.proxy(() => {})).toBeTruthy()
+})
