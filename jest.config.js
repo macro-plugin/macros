9b20a4d69680b858942635575a8ea920f901fdf9
voorjaar <felitas@zoho.com>
2023-04-20 03:49:39 +0800

    fix: ignore coverage from examples

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const fs = require("fs")
+
 /** @type {import("@jest/types").Config.InitialOptions} */
 module.exports = {
-  transform: { "^.+\\.(t|j)sx?$": "@swc/jest" },
-  moduleNameMapper: Object.fromEntries(require("fs").readdirSync("./packages").map(i => [[`^@macro-plugin/${i}`], [`<rootDir>/packages/${i}/src`]])),
+  transform: { "^.+\\.(t|j)s$": ["@swc/jest"] },
+  moduleNameMapper: Object.fromEntries(fs.readdirSync("./packages").map(i => [[`^@macro-plugin/${i}`], [`<rootDir>/packages/${i}/src`]])),
   collectCoverageFrom: ["packages/**/tests/**/*.test.ts"],
+  testPathIgnorePatterns: ["examples"]
 }
