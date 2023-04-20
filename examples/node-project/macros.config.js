f585fc3954eeb183763a4a253997b2e87317b9a0
voorjaar <felitas@zoho.com>
2023-04-20 10:44:10 +0800

    test: node register project

---
+++ examples/node-project/macros.config.js
@@ -0,0 +1,14 @@
+const { $Ast, $Eval, $Quote, createLitMacro, macro } = require("@macro-plugin/core")
+
+/** @type { import("@macro-plugin/core").Config } */
+module.exports = {
+  macros: [
+    macro,
+    $Eval,
+    $Ast,
+    $Quote,
+    createLitMacro({
+      __DEV__: false
+    })
+  ]
+}
---
+++ examples/node-project/src/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable no-console */
+
+console.log($Ast(1 + 2))
