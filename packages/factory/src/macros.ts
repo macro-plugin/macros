c3448efa7ddcfdd0b50eb9606ccc2b2f99adea28
voorjaar <felitas@zoho.com>
2023-04-23 05:02:32 +0800

    refactor: use array pattern unwrap args instead of array index

--- packages/factory/src/macros.ts
+++ packages/factory/src/macros.ts
@@ -35,47 +35,47 @@ export const $Void: ArrayExpression = {
   elements: []
 }
 
-export const $Identifier = createExprMacro("$Identifier", function (args) {
-  return createAst("Identifier", { value: args[0], optional: args[1] || $False })
+export const $Identifier = createExprMacro("$Identifier", function ([value, optional = $False]) {
+  return createAst("Identifier", { value, optional })
 }, "(value: string, optional?: boolean) => import(\"@swc/core\").Identifier").proxy(createIdentifier)
 
-export const $StringLiteral = createExprMacro("$StringLiteral", function (args) {
-  return createAst("StringLiteral", { value: args[0], raw: args[1] })
+export const $StringLiteral = createExprMacro("$StringLiteral", function ([value, raw]) {
+  return createAst("StringLiteral", { value, raw })
 }, "(value: string, raw?: string) => import(\"@swc/core\").StringLiteral").proxy(createStringLiteral)
 
-export const $NumericLiteral = createExprMacro("$NumericLiteral", function (args) {
-  return createAst("NumericLiteral", { value: args[0], raw: args[1] })
+export const $NumericLiteral = createExprMacro("$NumericLiteral", function ([value, raw]) {
+  return createAst("NumericLiteral", { value, raw })
 }, "(value: number, raw?: string) => import(\"@swc/core\").NumericLiteral").proxy(createNumericLiteral)
 
-export const $BigIntLiteral = createExprMacro("$BigIntLiteral", function (args) {
-  return createAst("BigIntLiteral", { value: args[0], raw: args[1] })
+export const $BigIntLiteral = createExprMacro("$BigIntLiteral", function ([value, raw]) {
+  return createAst("BigIntLiteral", { value, raw })
 }, "(value: bigint, raw?: string) => import(\"@swc/core\").BigIntLiteral").proxy(createBigIntLiteral)
 
-export const $BooleanLiteral = createExprMacro("$BooleanLiteral", function (args) {
-  return createAst("BooleanLiteral", { value: args[0] })
+export const $BooleanLiteral = createExprMacro("$BooleanLiteral", function ([value]) {
+  return createAst("BooleanLiteral", { value })
 }, "(value: boolean) => import(\"@swc/core\").BooleanLiteral").proxy(createBooleanLiteral)
 
-export const $RegExpLiteral = createExprMacro("$RegExpLiteral", function (args) {
-  return createAst("RegExpLiteral", { pattern: args[0], flags: args[1] })
+export const $RegExpLiteral = createExprMacro("$RegExpLiteral", function ([pattern, flags]) {
+  return createAst("RegExpLiteral", { pattern, flags })
 }, "(pattern: string, flags: string) => import(\"@swc/core\").RegExpLiteral").proxy(createRegExpLiteral)
 
-export const $Argument = createExprMacro("$Argument", function (args) {
+export const $Argument = createExprMacro("$Argument", function ([expression, spread]) {
   return this.parseExpr(`{
-    ${args[1] ? ('"spread": ' + JSON.stringify(span) + ",") : ""}
-    "expression": ${this.printExpr(args[0])}
+    ${spread ? ('"spread": ' + JSON.stringify(span) + ",") : ""}
+    "expression": ${this.printExpr(expression)}
   }`)
 }, "(expression: import(\"@swc/core\").Expression, spread?: boolean) => import(\"@swc/core\").Argument").proxy(createArgument)
 
-export const $CallExpression = createExprMacro("$CallExpression", function (args) {
+export const $CallExpression = createExprMacro("$CallExpression", function ([callee, callArgs = $Void, typeArguments]) {
   return createAst("CallExpression", {
-    callee: args[0],
-    arguments: args[1] || $Void,
-    typeArguments: args[2]
+    callee,
+    arguments: callArgs,
+    typeArguments
   })
 }, "(callee: import(\"@swc/core\").Expression | import(\"@swc/core\").Super | import(\"@swc/core\").Import, args?: import(\"@swc/core\").Argument[], typeArguments?: import(\"@swc/core\").TsTypeParameterInstantiation) => import(\"@swc/core\").CallExpression").proxy(createCallExpression)
 
-export const $Param = createExprMacro("$Param", function (args) {
-  return createAst("Parameter", { pat: args[0], decorators: args[1] })
+export const $Param = createExprMacro("$Param", function ([pat, decorators]) {
+  return createAst("Parameter", { pat, decorators })
 }, "(pat: import(\"@swc/core\").Pattern, decorators?: import(\"@swc/core\").Decorator[]) => import(\"@swc/core\").Param")
 
 export const $Invalid = createExprMacro("$Invalid", function () { return createAst("Invalid") }).proxy(createInvalid)
