9d9bbc243f5b75cf4b0ecc61f0496b125bf2367d
voorjaar <felitas@zoho.com>
2023-04-23 04:50:07 +0800

    fix: macros type emit

--- packages/factory/src/macros.ts
+++ packages/factory/src/macros.ts
@@ -37,34 +37,34 @@ export const $Void: ArrayExpression = {
 
 export const $Identifier = createExprMacro("$Identifier", function (args) {
   return createAst("Identifier", { value: args[0], optional: args[1] || $False })
-}, "(value: string, optional?: boolean) => Identifier").proxy(createIdentifier)
+}, "(value: string, optional?: boolean) => import(\"@swc/core\").Identifier").proxy(createIdentifier)
 
 export const $StringLiteral = createExprMacro("$StringLiteral", function (args) {
   return createAst("StringLiteral", { value: args[0], raw: args[1] })
-}, "(value: string, raw?: string) => StringLiteral").proxy(createStringLiteral)
+}, "(value: string, raw?: string) => import(\"@swc/core\").StringLiteral").proxy(createStringLiteral)
 
 export const $NumericLiteral = createExprMacro("$NumericLiteral", function (args) {
   return createAst("NumericLiteral", { value: args[0], raw: args[1] })
-}, "(value: number, raw?: string) => NumericLiteral").proxy(createNumericLiteral)
+}, "(value: number, raw?: string) => import(\"@swc/core\").NumericLiteral").proxy(createNumericLiteral)
 
 export const $BigIntLiteral = createExprMacro("$BigIntLiteral", function (args) {
   return createAst("BigIntLiteral", { value: args[0], raw: args[1] })
-}, "(value: bigint, raw?: string) => BigIntLiteral").proxy(createBigIntLiteral)
+}, "(value: bigint, raw?: string) => import(\"@swc/core\").BigIntLiteral").proxy(createBigIntLiteral)
 
 export const $BooleanLiteral = createExprMacro("$BooleanLiteral", function (args) {
   return createAst("BooleanLiteral", { value: args[0] })
-}, "(value: boolean) => BooleanLiteral").proxy(createBooleanLiteral)
+}, "(value: boolean) => import(\"@swc/core\").BooleanLiteral").proxy(createBooleanLiteral)
 
 export const $RegExpLiteral = createExprMacro("$RegExpLiteral", function (args) {
   return createAst("RegExpLiteral", { pattern: args[0], flags: args[1] })
-}, "(pattern: string, flags: string) => RegExpLiteral").proxy(createRegExpLiteral)
+}, "(pattern: string, flags: string) => import(\"@swc/core\").RegExpLiteral").proxy(createRegExpLiteral)
 
 export const $Argument = createExprMacro("$Argument", function (args) {
   return this.parseExpr(`{
     ${args[1] ? ('"spread": ' + JSON.stringify(span) + ",") : ""}
     "expression": ${this.printExpr(args[0])}
   }`)
-}, "(expression: Expression, spread?: boolean) => Argument").proxy(createArgument)
+}, "(expression: import(\"@swc/core\").Expression, spread?: boolean) => import(\"@swc/core\").Argument").proxy(createArgument)
 
 export const $CallExpression = createExprMacro("$CallExpression", function (args) {
   return createAst("CallExpression", {
@@ -72,11 +72,11 @@ export const $CallExpression = createExprMacro("$CallExpression", function (args
     arguments: args[1] || $Void,
     typeArguments: args[2]
   })
-}, "(callee: Expression | Super | Import, args?: Argument[], typeArguments?: TsTypeParameterInstantiation) => CallExpression").proxy(createCallExpression)
+}, "(callee: import(\"@swc/core\").Expression | import(\"@swc/core\").Super | import(\"@swc/core\").Import, args?: import(\"@swc/core\").Argument[], typeArguments?: import(\"@swc/core\").TsTypeParameterInstantiation) => import(\"@swc/core\").CallExpression").proxy(createCallExpression)
 
 export const $Param = createExprMacro("$Param", function (args) {
   return createAst("Parameter", { pat: args[0], decorators: args[1] })
-}, "(pat: Pattern, decorators?: Decorator[]) => Param")
+}, "(pat: import(\"@swc/core\").Pattern, decorators?: import(\"@swc/core\").Decorator[]) => import(\"@swc/core\").Param")
 
 export const $Invalid = createExprMacro("$Invalid", function () { return createAst("Invalid") }).proxy(createInvalid)
 
